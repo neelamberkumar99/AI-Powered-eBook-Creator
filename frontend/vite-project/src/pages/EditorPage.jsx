a12cec502ba130ddf2246788751442faae13f6d9
nilu <neelambarkumar966@gmail.com>
2026-06-12 10:08:40 +0530

    commit

---
+++ frontend/vite-project/src/pages/EditorPage.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState, useRef } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import {
+  Sparkles,
+  FileDown,
+  Save,
+  Menu,
+  X,
+  Edit,
+  NotebookText,
+  ChevronDown,
+  FileText,
+} from "lucide-react";
+import { arrayMove } from "@dnd-kit/sortable";
+
+import axiosInstance from "../utils/axiosInstance";
+import { API_PATHS } from "../utils/apiPaths";
+import Dropdown, { DropdownItem } from "../components/ui/Dropdown";
+import InputField from "../components/ui/InputField";
+import Button from "../components/ui/Button";
+import Modal from "../components/ui/Modal";
+import SelectField from "../components/ui/SelectField";
+
+const EditorPage = () => {
+  return (
+    <div>EditorPage</div>
+  );
+};
+
+export default EditorPage;
