0251f21838a4c965a492bf04eb32289b5757594d
nilu <neelambarkumar966@gmail.com>
2026-06-15 00:17:32 +0530

    firstcommit

--- frontend/vite-project/src/pages/LandingPages.jsx
+++ frontend/vite-project/src/pages/LandingPages.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/layout/Navbar";
 import Hero from "../components/landing/Hero";
+import Features from "../components/landing/Features";
 
 const LandingPages = () => {
   return (
-    <div>
+    <div className="mb-[100vh]">
       <Navbar />
       <Hero />
+      <Features />
     </div>
   );
 };
