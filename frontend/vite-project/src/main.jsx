467c15adcf07a3d05703e46917da2add2a6926f2
nilu <neelambarkumar966@gmail.com>
2026-06-15 19:04:36 +0530

    nbvn

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -7,9 +7,11 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
     <BrowserRouter>
     <App />
     <Toaster position='bottom-right' reverseOrder={false}/>
     </BrowserRouter>
+    </AuthProvider>
   </StrictMode>,
 )
